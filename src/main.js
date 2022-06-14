76564883a36c08194c41932c29682a57125a7dd2
smithst <62660737+smithst18@users.noreply.github.com>
2022-06-14 10:13:08 -0400

    clean dependencys

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import "@/styles/styles.scss";
-import "jquery";
 
 createApp(App).use(store).use(router).mount('#app')
